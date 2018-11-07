a9f815942e15256582db6a3c059bfeb82561fec0
unknown <schifferjulian@gmail.com>
2018-11-07 00:54:57 -0300

    Agrego las compras, y libero el select para toda la tabla maestra

--- data/Migracion_de_datos.sql
+++ data/Migracion_de_datos.sql
@@ -2,6 +2,9 @@
 AS
 BEGIN
 	/* Limpio las tablas para no tener errores despues */
+	DELETE FROM [EL_REJUNTE].[Ubicacion_Compra]
+	DELETE FROM [EL_REJUNTE].[Compra]
+	DELETE FROM [EL_REJUNTE].[Ubicacion]
 	DELETE FROM [EL_REJUNTE].[Cliente]
 	DELETE FROM [EL_REJUNTE].[Empresa]
 	DELETE FROM [EL_REJUNTE].[Espectaculo]
@@ -44,6 +47,9 @@ BEGIN
 	DECLARE @Cli_Piso numeric(18,0)
 	DECLARE @Cli_Depto nvarchar(255)
 	DECLARE @Cli_Cod_Postal nvarchar(255)
+	/* Variables para las Compras */
+	DECLARE @Compra_Fecha datetime
+	DECLARE @Compra_Cantidad numeric(18,0)
 	/* Otras Variables */
 	DECLARE @ID_Direccion INT
 	DECLARE @ID_Cliente INT
@@ -51,17 +57,14 @@ BEGIN
 	DECLARE @ID_Rubro INT
 	DECLARE @ID_Espectaculo INT
 	DECLARE @ID_Ubicacion INT
+	DECLARE @ID_Compra INT
 	
 	DECLARE c_maestro CURSOR FOR
-		SELECT TOP 1000 gd.Espec_Empresa_Razon_Social, gd.Espec_Empresa_Cuit, gd.Espec_Empresa_Fecha_Creacion, gd.Espec_Empresa_Mail, gd.Espec_Empresa_Dom_Calle, gd.Espec_Empresa_Nro_Calle, gd.Espec_Empresa_Piso, gd.Espec_Empresa_Depto, gd.Espec_Empresa_Cod_Postal, gd.Espectaculo_Cod, gd.Espectaculo_Descripcion, gd.Espectaculo_Fecha, gd.Espectaculo_Fecha_Venc, gd.Espectaculo_Rubro_Descripcion, gd.Espectaculo_Estado, gd.Ubicacion_Fila, gd.Ubicacion_Asiento, gd.Ubicacion_Sin_numerar, gd.Ubicacion_Precio, gd.Ubicacion_Tipo_Codigo, gd.Ubicacion_Tipo_Descripcion, gd.Cli_Dni, gd.Cli_Apeliido, gd.Cli_Nombre, gd.Cli_Fecha_Nac, gd.Cli_Mail, gd.Cli_Dom_Calle, gd.Cli_Nro_Calle, gd.Cli_Piso, gd.Cli_Depto, gd.Cli_Cod_Postal
+		SELECT DISTINCT gd.Espec_Empresa_Razon_Social, gd.Espec_Empresa_Cuit, gd.Espec_Empresa_Fecha_Creacion, gd.Espec_Empresa_Mail, gd.Espec_Empresa_Dom_Calle, gd.Espec_Empresa_Nro_Calle, gd.Espec_Empresa_Piso, gd.Espec_Empresa_Depto, gd.Espec_Empresa_Cod_Postal, gd.Espectaculo_Cod, gd.Espectaculo_Descripcion, gd.Espectaculo_Fecha, gd.Espectaculo_Fecha_Venc, gd.Espectaculo_Rubro_Descripcion, gd.Espectaculo_Estado, gd.Ubicacion_Fila, gd.Ubicacion_Asiento, gd.Ubicacion_Sin_numerar, gd.Ubicacion_Precio, gd.Ubicacion_Tipo_Codigo, gd.Ubicacion_Tipo_Descripcion, gd.Cli_Dni, gd.Cli_Apeliido, gd.Cli_Nombre, gd.Cli_Fecha_Nac, gd.Cli_Mail, gd.Cli_Dom_Calle, gd.Cli_Nro_Calle, gd.Cli_Piso, gd.Cli_Depto, gd.Cli_Cod_Postal, gd.Compra_Fecha, gd.Compra_Cantidad
 		FROM gd_esquema.Maestra gd
-		WHERE   gd.Cli_Nombre IS NOT NULL AND
-				gd.Cli_Apeliido IS NOT NULL AND
-				gd.Cli_Dni IS NOT NULL AND
-				gd.Cli_Mail IS NOT NULL
 			
 	OPEN c_maestro
-	FETCH NEXT FROM c_maestro INTO @Espec_Empresa_Razon_Social, @Espec_Empresa_Cuit, @Espec_Empresa_Fecha_Creacion, @Espec_Empresa_Mail, @Espec_Empresa_Dom_Calle, @Espec_Empresa_Nro_Calle, @Espec_Empresa_Piso, @Espec_Empresa_Depto, @Espec_Empresa_Cod_Postal, @Espectaculo_Cod, @Espectaculo_Descripcion, @Espectaculo_Fecha, @Espectaculo_Fecha_Venc, @Espectaculo_Rubro_Descripcion, @Espectaculo_Estado, @Ubicacion_Fila ,@Ubicacion_Asiento ,@Ubicacion_Sin_numerar ,@Ubicacion_Precio ,@Ubicacion_Tipo_Codigo ,@Ubicacion_Tipo_Descripcion, @Cli_Dni, @Cli_Apellido, @Cli_Nombre, @Cli_Fecha_Nac, @Cli_Mail, @Cli_Dom_Calle, @Cli_Nro_Calle,@Cli_Piso, @Cli_Depto, @Cli_Cod_Postal
+	FETCH NEXT FROM c_maestro INTO @Espec_Empresa_Razon_Social, @Espec_Empresa_Cuit, @Espec_Empresa_Fecha_Creacion, @Espec_Empresa_Mail, @Espec_Empresa_Dom_Calle, @Espec_Empresa_Nro_Calle, @Espec_Empresa_Piso, @Espec_Empresa_Depto, @Espec_Empresa_Cod_Postal, @Espectaculo_Cod, @Espectaculo_Descripcion, @Espectaculo_Fecha, @Espectaculo_Fecha_Venc, @Espectaculo_Rubro_Descripcion, @Espectaculo_Estado, @Ubicacion_Fila ,@Ubicacion_Asiento ,@Ubicacion_Sin_numerar ,@Ubicacion_Precio ,@Ubicacion_Tipo_Codigo ,@Ubicacion_Tipo_Descripcion, @Cli_Dni, @Cli_Apellido, @Cli_Nombre, @Cli_Fecha_Nac, @Cli_Mail, @Cli_Dom_Calle, @Cli_Nro_Calle,@Cli_Piso, @Cli_Depto, @Cli_Cod_Postal, @Compra_Fecha, @Compra_Cantidad
 
 	WHILE(@@FETCH_STATUS=0)
 	BEGIN
@@ -105,7 +108,7 @@ BEGIN
 			  clie_email = @Cli_Mail AND
 			  clie_direccion_id = @ID_Direccion AND
 			  clie_fecha_nacimiento = @Cli_Fecha_Nac
-		IF(@ID_Cliente IS NULL)
+		IF(@ID_Cliente IS NULL AND @Cli_Nombre IS NOT NULL AND @Cli_Apellido IS NOT NULL)
 		BEGIN
 			/* Hago el Insert de los Clientes, con sus respectivas Direcciones */
 			INSERT INTO EL_REJUNTE.Cliente (clie_nombre, clie_apellido, clie_tipo_documento, clie_documento, clie_cuil, clie_email, clie_telefono, clie_direccion_id, clie_fecha_nacimiento,clie_fecha_creacion, clie_tarjeta_id, clie_habilitado, clie_usuario_id)
@@ -182,6 +185,45 @@ BEGIN
 			INSERT INTO EL_REJUNTE.Ubicacion (ubica_fila , ubica_asiento, ubica_sin_numerar, ubica_precio, ubica_tipo_codigo, ubica_tipo_descripcion,ubica_facturada)
 			VALUES (@Ubicacion_Fila, @Ubicacion_Asiento, @Ubicacion_Sin_numerar, @Ubicacion_Precio, @Ubicacion_Tipo_Codigo, @Ubicacion_Tipo_Descripcion, 0)
 		END
+		/*Verifico si la Compra ya existe o si es Compra Nueva*/
+		SELECT @ID_Ubicacion = ubica_id
+		FROM Ubicacion
+		WHERE ubica_fila = @Ubicacion_Fila AND
+			  ubica_asiento = @Ubicacion_Asiento AND
+			  ubica_sin_numerar = @Ubicacion_Sin_numerar AND
+			  ubica_precio = @Ubicacion_Precio AND
+			  ubica_tipo_codigo = @Ubicacion_Tipo_Codigo AND 
+			  ubica_tipo_descripcion = @Ubicacion_Tipo_Descripcion
+			  
+		SELECT @ID_Cliente = clie_id
+		FROM Cliente
+		WHERE clie_nombre = @Cli_Nombre AND
+			  clie_apellido = @Cli_Apellido AND
+			  clie_documento = @Cli_Dni AND
+			  clie_email = @Cli_Mail AND
+			  clie_direccion_id = @ID_Direccion AND
+			  clie_fecha_nacimiento = @Cli_Fecha_Nac
+		
+		SELECT @ID_Compra = compra_id
+		FROM Compra
+		WHERE compra_fecha = @Compra_Fecha AND
+			  compra_cantidad = @Compra_Cantidad AND
+			  compra_cliente_id = @ID_Cliente
+		IF(@ID_Compra IS NULL AND @ID_Cliente IS NOT NULL AND @ID_Ubicacion IS NOT NULL)
+		BEGIN				  
+			INSERT INTO EL_REJUNTE.Compra (compra_fecha , compra_cantidad, compra_cliente_id)
+			VALUES (@Compra_Fecha, @Compra_Cantidad, @ID_Cliente)
+			/* Agrego el campo de la tabla intermedia */
+			
+			SELECT @ID_Compra = compra_id
+			FROM Compra
+			WHERE compra_fecha = @Compra_Fecha AND
+				  compra_cantidad = @Compra_Cantidad AND
+				  compra_cliente_id = @ID_Cliente
+		
+			INSERT INTO EL_REJUNTE.Ubicacion_Compra(ubica_id, compra_id)
+			VALUES(@ID_Ubicacion, @ID_Compra)
+		END
 	
 	/* Termina el clasificado de Ubicaciones */
 	
@@ -193,10 +235,11 @@ BEGIN
 		SET @ID_Rubro = NULL
 		SET @ID_Espectaculo = NULL
 		SET @ID_Ubicacion = NULL
+		SET @ID_Compra = NULL
 		
 		
 	/* Tomo la siguiente Linea */
-		FETCH NEXT FROM c_maestro INTO @Espec_Empresa_Razon_Social, @Espec_Empresa_Cuit, @Espec_Empresa_Fecha_Creacion, @Espec_Empresa_Mail, @Espec_Empresa_Dom_Calle, @Espec_Empresa_Nro_Calle, @Espec_Empresa_Piso, @Espec_Empresa_Depto, @Espec_Empresa_Cod_Postal, @Espectaculo_Cod, @Espectaculo_Descripcion, @Espectaculo_Fecha, @Espectaculo_Fecha_Venc, @Espectaculo_Rubro_Descripcion, @Espectaculo_Estado, @Ubicacion_Fila ,@Ubicacion_Asiento ,@Ubicacion_Sin_numerar ,@Ubicacion_Precio ,@Ubicacion_Tipo_Codigo ,@Ubicacion_Tipo_Descripcion, @Cli_Dni, @Cli_Apellido, @Cli_Nombre, @Cli_Fecha_Nac, @Cli_Mail, @Cli_Dom_Calle, @Cli_Nro_Calle,@Cli_Piso, @Cli_Depto, @Cli_Cod_Postal
+		FETCH NEXT FROM c_maestro INTO @Espec_Empresa_Razon_Social, @Espec_Empresa_Cuit, @Espec_Empresa_Fecha_Creacion, @Espec_Empresa_Mail, @Espec_Empresa_Dom_Calle, @Espec_Empresa_Nro_Calle, @Espec_Empresa_Piso, @Espec_Empresa_Depto, @Espec_Empresa_Cod_Postal, @Espectaculo_Cod, @Espectaculo_Descripcion, @Espectaculo_Fecha, @Espectaculo_Fecha_Venc, @Espectaculo_Rubro_Descripcion, @Espectaculo_Estado, @Ubicacion_Fila ,@Ubicacion_Asiento ,@Ubicacion_Sin_numerar ,@Ubicacion_Precio ,@Ubicacion_Tipo_Codigo ,@Ubicacion_Tipo_Descripcion, @Cli_Dni, @Cli_Apellido, @Cli_Nombre, @Cli_Fecha_Nac, @Cli_Mail, @Cli_Dom_Calle, @Cli_Nro_Calle,@Cli_Piso, @Cli_Depto, @Cli_Cod_Postal, @Compra_Fecha, @Compra_Cantidad
 	END
 	/* Termino de recorrer el Cursor, lo cierro y libero la memoria */
 	CLOSE c_maestro
