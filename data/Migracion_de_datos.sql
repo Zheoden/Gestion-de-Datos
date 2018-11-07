44148e85ae815eaf163334833f89ed7ad2e61a91
unknown <schifferjulian@gmail.com>
2018-11-07 01:53:23 -0300

    Termino la primera version del procedure, falta arreglar la performance, pero migra todos los datos

--- data/Migracion_de_datos.sql
+++ data/Migracion_de_datos.sql
@@ -3,8 +3,10 @@ AS
 BEGIN
 	/* Limpio las tablas para no tener errores despues */
 	DELETE FROM [EL_REJUNTE].[Ubicacion_Compra]
+	DELETE FROM [EL_REJUNTE].[Item_Factura]
 	DELETE FROM [EL_REJUNTE].[Compra]
 	DELETE FROM [EL_REJUNTE].[Ubicacion]
+	DELETE FROM [EL_REJUNTE].[Factura]
 	DELETE FROM [EL_REJUNTE].[Cliente]
 	DELETE FROM [EL_REJUNTE].[Empresa]
 	DELETE FROM [EL_REJUNTE].[Espectaculo]
@@ -50,6 +52,15 @@ BEGIN
 	/* Variables para las Compras */
 	DECLARE @Compra_Fecha datetime
 	DECLARE @Compra_Cantidad numeric(18,0)
+	/* Variables para los Items */
+	DECLARE @Item_Factura_Monto numeric(18,0)
+	DECLARE @Item_Factura_Cantidad numeric(18,0)
+	DECLARE @Item_Factura_Descripcion nvarchar(60)
+	/* Variables para las Facturas */
+	DECLARE @Factura_Nro numeric(18,0)
+	DECLARE @Factura_Fecha datetime
+	DECLARE @Factura_Total numeric(18,2)
+	DECLARE @Forma_Pago_Desc nvarchar(255)
 	/* Otras Variables */
 	DECLARE @ID_Direccion INT
 	DECLARE @ID_Cliente INT
@@ -58,13 +69,15 @@ BEGIN
 	DECLARE @ID_Espectaculo INT
 	DECLARE @ID_Ubicacion INT
 	DECLARE @ID_Compra INT
+	DECLARE @ID_Item INT
+	DECLARE @ID_Factura INT
 	
 	DECLARE c_maestro CURSOR FOR
-		SELECT DISTINCT gd.Espec_Empresa_Razon_Social, gd.Espec_Empresa_Cuit, gd.Espec_Empresa_Fecha_Creacion, gd.Espec_Empresa_Mail, gd.Espec_Empresa_Dom_Calle, gd.Espec_Empresa_Nro_Calle, gd.Espec_Empresa_Piso, gd.Espec_Empresa_Depto, gd.Espec_Empresa_Cod_Postal, gd.Espectaculo_Cod, gd.Espectaculo_Descripcion, gd.Espectaculo_Fecha, gd.Espectaculo_Fecha_Venc, gd.Espectaculo_Rubro_Descripcion, gd.Espectaculo_Estado, gd.Ubicacion_Fila, gd.Ubicacion_Asiento, gd.Ubicacion_Sin_numerar, gd.Ubicacion_Precio, gd.Ubicacion_Tipo_Codigo, gd.Ubicacion_Tipo_Descripcion, gd.Cli_Dni, gd.Cli_Apeliido, gd.Cli_Nombre, gd.Cli_Fecha_Nac, gd.Cli_Mail, gd.Cli_Dom_Calle, gd.Cli_Nro_Calle, gd.Cli_Piso, gd.Cli_Depto, gd.Cli_Cod_Postal, gd.Compra_Fecha, gd.Compra_Cantidad
+		SELECT DISTINCT TOP 150000 gd.Espec_Empresa_Razon_Social, gd.Espec_Empresa_Cuit, gd.Espec_Empresa_Fecha_Creacion, gd.Espec_Empresa_Mail, gd.Espec_Empresa_Dom_Calle, gd.Espec_Empresa_Nro_Calle, gd.Espec_Empresa_Piso, gd.Espec_Empresa_Depto, gd.Espec_Empresa_Cod_Postal, gd.Espectaculo_Cod, gd.Espectaculo_Descripcion, gd.Espectaculo_Fecha, gd.Espectaculo_Fecha_Venc, gd.Espectaculo_Rubro_Descripcion, gd.Espectaculo_Estado, gd.Ubicacion_Fila, gd.Ubicacion_Asiento, gd.Ubicacion_Sin_numerar, gd.Ubicacion_Precio, gd.Ubicacion_Tipo_Codigo, gd.Ubicacion_Tipo_Descripcion, gd.Cli_Dni, gd.Cli_Apeliido, gd.Cli_Nombre, gd.Cli_Fecha_Nac, gd.Cli_Mail, gd.Cli_Dom_Calle, gd.Cli_Nro_Calle, gd.Cli_Piso, gd.Cli_Depto, gd.Cli_Cod_Postal, gd.Compra_Fecha, gd.Compra_Cantidad, gd.Item_Factura_Monto, gd.Item_Factura_Cantidad, gd.Item_Factura_Descripcion, gd.Factura_Nro, gd.Factura_Fecha, gd.Factura_Total, gd.Forma_Pago_Desc
 		FROM gd_esquema.Maestra gd
 			
 	OPEN c_maestro
-	FETCH NEXT FROM c_maestro INTO @Espec_Empresa_Razon_Social, @Espec_Empresa_Cuit, @Espec_Empresa_Fecha_Creacion, @Espec_Empresa_Mail, @Espec_Empresa_Dom_Calle, @Espec_Empresa_Nro_Calle, @Espec_Empresa_Piso, @Espec_Empresa_Depto, @Espec_Empresa_Cod_Postal, @Espectaculo_Cod, @Espectaculo_Descripcion, @Espectaculo_Fecha, @Espectaculo_Fecha_Venc, @Espectaculo_Rubro_Descripcion, @Espectaculo_Estado, @Ubicacion_Fila ,@Ubicacion_Asiento ,@Ubicacion_Sin_numerar ,@Ubicacion_Precio ,@Ubicacion_Tipo_Codigo ,@Ubicacion_Tipo_Descripcion, @Cli_Dni, @Cli_Apellido, @Cli_Nombre, @Cli_Fecha_Nac, @Cli_Mail, @Cli_Dom_Calle, @Cli_Nro_Calle,@Cli_Piso, @Cli_Depto, @Cli_Cod_Postal, @Compra_Fecha, @Compra_Cantidad
+	FETCH NEXT FROM c_maestro INTO @Espec_Empresa_Razon_Social, @Espec_Empresa_Cuit, @Espec_Empresa_Fecha_Creacion, @Espec_Empresa_Mail, @Espec_Empresa_Dom_Calle, @Espec_Empresa_Nro_Calle, @Espec_Empresa_Piso, @Espec_Empresa_Depto, @Espec_Empresa_Cod_Postal, @Espectaculo_Cod, @Espectaculo_Descripcion, @Espectaculo_Fecha, @Espectaculo_Fecha_Venc, @Espectaculo_Rubro_Descripcion, @Espectaculo_Estado, @Ubicacion_Fila ,@Ubicacion_Asiento ,@Ubicacion_Sin_numerar ,@Ubicacion_Precio ,@Ubicacion_Tipo_Codigo ,@Ubicacion_Tipo_Descripcion, @Cli_Dni, @Cli_Apellido, @Cli_Nombre, @Cli_Fecha_Nac, @Cli_Mail, @Cli_Dom_Calle, @Cli_Nro_Calle,@Cli_Piso, @Cli_Depto, @Cli_Cod_Postal, @Compra_Fecha, @Compra_Cantidad, @Item_Factura_Monto, @Item_Factura_Cantidad, @Item_Factura_Descripcion, @Factura_Nro, @Factura_Fecha, @Factura_Total, @Forma_Pago_Desc
 
 	WHILE(@@FETCH_STATUS=0)
 	BEGIN
@@ -186,46 +199,119 @@ BEGIN
 			VALUES (@Ubicacion_Fila, @Ubicacion_Asiento, @Ubicacion_Sin_numerar, @Ubicacion_Precio, @Ubicacion_Tipo_Codigo, @Ubicacion_Tipo_Descripcion, 0)
 		END
 		/*Verifico si la Compra ya existe o si es Compra Nueva*/
-		SELECT @ID_Ubicacion = ubica_id
-		FROM Ubicacion
-		WHERE ubica_fila = @Ubicacion_Fila AND
-			  ubica_asiento = @Ubicacion_Asiento AND
-			  ubica_sin_numerar = @Ubicacion_Sin_numerar AND
-			  ubica_precio = @Ubicacion_Precio AND
-			  ubica_tipo_codigo = @Ubicacion_Tipo_Codigo AND 
-			  ubica_tipo_descripcion = @Ubicacion_Tipo_Descripcion
-			  
-		SELECT @ID_Cliente = clie_id
-		FROM Cliente
-		WHERE clie_nombre = @Cli_Nombre AND
-			  clie_apellido = @Cli_Apellido AND
-			  clie_documento = @Cli_Dni AND
-			  clie_email = @Cli_Mail AND
-			  clie_direccion_id = @ID_Direccion AND
-			  clie_fecha_nacimiento = @Cli_Fecha_Nac
 		
 		SELECT @ID_Compra = compra_id
 		FROM Compra
 		WHERE compra_fecha = @Compra_Fecha AND
 			  compra_cantidad = @Compra_Cantidad AND
 			  compra_cliente_id = @ID_Cliente
-		IF(@ID_Compra IS NULL AND @ID_Cliente IS NOT NULL AND @ID_Ubicacion IS NOT NULL)
-		BEGIN				  
-			INSERT INTO EL_REJUNTE.Compra (compra_fecha , compra_cantidad, compra_cliente_id)
-			VALUES (@Compra_Fecha, @Compra_Cantidad, @ID_Cliente)
-			/* Agrego el campo de la tabla intermedia */
-			
-			SELECT @ID_Compra = compra_id
-			FROM Compra
-			WHERE compra_fecha = @Compra_Fecha AND
-				  compra_cantidad = @Compra_Cantidad AND
-				  compra_cliente_id = @ID_Cliente
+			  
+		IF(@ID_Compra IS NULL)
+		BEGIN				
+
+			SELECT @ID_Cliente = clie_id
+			FROM Cliente
+			WHERE clie_nombre = @Cli_Nombre AND
+				clie_apellido = @Cli_Apellido AND
+				clie_documento = @Cli_Dni AND
+				clie_email = @Cli_Mail AND
+				clie_direccion_id = @ID_Direccion AND
+				clie_fecha_nacimiento = @Cli_Fecha_Nac
+			  
+			SELECT @ID_Ubicacion = ubica_id
+			FROM Ubicacion
+			WHERE ubica_fila = @Ubicacion_Fila AND
+			  ubica_asiento = @Ubicacion_Asiento AND
+			  ubica_sin_numerar = @Ubicacion_Sin_numerar AND
+			  ubica_precio = @Ubicacion_Precio AND
+			  ubica_tipo_codigo = @Ubicacion_Tipo_Codigo AND 
+			  ubica_tipo_descripcion = @Ubicacion_Tipo_Descripcion
+			  
+			IF(@ID_Cliente IS NOT NULL AND @ID_Ubicacion IS NOT NULL)
+			BEGIN
+				INSERT INTO EL_REJUNTE.Compra (compra_fecha , compra_cantidad, compra_cliente_id)
+				VALUES (@Compra_Fecha, @Compra_Cantidad, @ID_Cliente)
+				/* Agrego el campo de la tabla intermedia */
+	
+				SELECT @ID_Compra = compra_id
+				FROM Compra
+				WHERE compra_fecha = @Compra_Fecha AND
+					compra_cantidad = @Compra_Cantidad AND
+					compra_cliente_id = @ID_Cliente
 		
-			INSERT INTO EL_REJUNTE.Ubicacion_Compra(ubica_id, compra_id)
-			VALUES(@ID_Ubicacion, @ID_Compra)
+				INSERT INTO EL_REJUNTE.Ubicacion_Compra(ubica_id, compra_id)
+				VALUES(@ID_Ubicacion, @ID_Compra)
+			END
 		END
 	
 	/* Termina el clasificado de Ubicaciones */
+	
+		SELECT @ID_Factura = fact_id
+		FROM Factura
+		WHERE fact_nro = @Factura_Nro AND
+			  fact_fecha = @Factura_Fecha AND
+			  fact_total = @Factura_Total AND
+			  fact_pago_desc = @Forma_Pago_Desc
+			  
+		IF(@ID_Factura IS NULL )
+		BEGIN
+			SELECT @ID_Cliente = clie_id
+			FROM Cliente
+			WHERE clie_nombre = @Cli_Nombre AND
+				clie_apellido = @Cli_Apellido AND
+				clie_documento = @Cli_Dni AND
+				clie_email = @Cli_Mail AND
+				clie_direccion_id = @ID_Direccion AND
+				clie_fecha_nacimiento = @Cli_Fecha_Nac
+				
+			SELECT @ID_Empresa = empre_id
+			FROM Empresa
+			WHERE empre_razon_social = @Espec_Empresa_Razon_Social AND
+				empre_cuit = @Espec_Empresa_Cuit AND
+				empre_fecha_creacion = @Espec_Empresa_Fecha_Creacion AND
+				empre_mail = @Espec_Empresa_Mail AND
+				empre_direccion_id = @ID_Direccion
+				
+			IF(@ID_Cliente IS NOT NULL AND @ID_Empresa IS NOT NULL AND @Factura_Nro IS NOT NULL)
+			BEGIN
+				INSERT INTO EL_REJUNTE.Factura (fact_nro , fact_fecha, fact_total, fact_pago_desc, fact_cliente_id, fact_empresa_id)
+				VALUES (@Factura_Nro, @Factura_Fecha, @Factura_Total, @Forma_Pago_Desc, @ID_Cliente, @ID_Empresa)
+			END
+		END
+	
+	/* Termina el clasificado de Facturacion */
+	
+		SELECT @ID_Item = item_id
+		FROM Item_Factura
+		WHERE item_monto = @Item_Factura_Monto AND
+			  item_cantidad = @Item_Factura_Cantidad AND
+			  item_descripcion = @Item_Factura_Descripcion	
+	
+		IF(@ID_Item IS NULL )
+		BEGIN
+			SELECT @ID_Factura = fact_id
+			FROM Factura
+			WHERE fact_nro = @Factura_Nro AND
+				fact_fecha = @Factura_Fecha AND
+				fact_total = @Factura_Total AND
+				fact_pago_desc = @Forma_Pago_Desc
+				
+			SELECT @ID_Ubicacion = ubica_id
+			FROM Ubicacion
+			WHERE ubica_fila = @Ubicacion_Fila AND
+			  ubica_asiento = @Ubicacion_Asiento AND
+			  ubica_sin_numerar = @Ubicacion_Sin_numerar AND
+			  ubica_precio = @Ubicacion_Precio AND
+			  ubica_tipo_codigo = @Ubicacion_Tipo_Codigo AND 
+			  ubica_tipo_descripcion = @Ubicacion_Tipo_Descripcion
+				
+			IF(@ID_Factura IS NOT NULL AND @ID_Ubicacion IS NOT NULL)
+			BEGIN
+				INSERT INTO EL_REJUNTE.Item_Factura (item_monto , item_cantidad, item_descripcion, item_factura_id, item_ubicacion_id)
+				VALUES (@Item_Factura_Monto, @Item_Factura_Cantidad, @Item_Factura_Descripcion, @ID_Factura, @ID_Ubicacion)
+			END
+		END
+	
 	
 	/* Reinicio las variables Unicas */
 		SET @ID_Direccion = NULL
@@ -236,10 +322,11 @@ BEGIN
 		SET @ID_Espectaculo = NULL
 		SET @ID_Ubicacion = NULL
 		SET @ID_Compra = NULL
-		
+		SET @ID_Item = NULL
+		SET @ID_Factura = NULL
 		
 	/* Tomo la siguiente Linea */
-		FETCH NEXT FROM c_maestro INTO @Espec_Empresa_Razon_Social, @Espec_Empresa_Cuit, @Espec_Empresa_Fecha_Creacion, @Espec_Empresa_Mail, @Espec_Empresa_Dom_Calle, @Espec_Empresa_Nro_Calle, @Espec_Empresa_Piso, @Espec_Empresa_Depto, @Espec_Empresa_Cod_Postal, @Espectaculo_Cod, @Espectaculo_Descripcion, @Espectaculo_Fecha, @Espectaculo_Fecha_Venc, @Espectaculo_Rubro_Descripcion, @Espectaculo_Estado, @Ubicacion_Fila ,@Ubicacion_Asiento ,@Ubicacion_Sin_numerar ,@Ubicacion_Precio ,@Ubicacion_Tipo_Codigo ,@Ubicacion_Tipo_Descripcion, @Cli_Dni, @Cli_Apellido, @Cli_Nombre, @Cli_Fecha_Nac, @Cli_Mail, @Cli_Dom_Calle, @Cli_Nro_Calle,@Cli_Piso, @Cli_Depto, @Cli_Cod_Postal, @Compra_Fecha, @Compra_Cantidad
+		FETCH NEXT FROM c_maestro INTO @Espec_Empresa_Razon_Social, @Espec_Empresa_Cuit, @Espec_Empresa_Fecha_Creacion, @Espec_Empresa_Mail, @Espec_Empresa_Dom_Calle, @Espec_Empresa_Nro_Calle, @Espec_Empresa_Piso, @Espec_Empresa_Depto, @Espec_Empresa_Cod_Postal, @Espectaculo_Cod, @Espectaculo_Descripcion, @Espectaculo_Fecha, @Espectaculo_Fecha_Venc, @Espectaculo_Rubro_Descripcion, @Espectaculo_Estado, @Ubicacion_Fila ,@Ubicacion_Asiento ,@Ubicacion_Sin_numerar ,@Ubicacion_Precio ,@Ubicacion_Tipo_Codigo ,@Ubicacion_Tipo_Descripcion, @Cli_Dni, @Cli_Apellido, @Cli_Nombre, @Cli_Fecha_Nac, @Cli_Mail, @Cli_Dom_Calle, @Cli_Nro_Calle,@Cli_Piso, @Cli_Depto, @Cli_Cod_Postal, @Compra_Fecha, @Compra_Cantidad, @Item_Factura_Monto, @Item_Factura_Cantidad, @Item_Factura_Descripcion, @Factura_Nro, @Factura_Fecha, @Factura_Total, @Forma_Pago_Desc
 	END
 	/* Termino de recorrer el Cursor, lo cierro y libero la memoria */
 	CLOSE c_maestro
