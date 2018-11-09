288cbd50aa9c0674367ff97248554e2ceb677c78
unknown <schifferjulian@gmail.com>
2018-11-09 01:40:22 -0300

    Optimizo |

--- data/Migracion_de_datos.sql
+++ data/Migracion_de_datos.sql
@@ -73,7 +73,7 @@ BEGIN
 	DECLARE @ID_Factura INT
 	
 	DECLARE c_maestro CURSOR FOR
-		SELECT DISTINCT TOP 150000 gd.Espec_Empresa_Razon_Social, gd.Espec_Empresa_Cuit, gd.Espec_Empresa_Fecha_Creacion, gd.Espec_Empresa_Mail, gd.Espec_Empresa_Dom_Calle, gd.Espec_Empresa_Nro_Calle, gd.Espec_Empresa_Piso, gd.Espec_Empresa_Depto, gd.Espec_Empresa_Cod_Postal, gd.Espectaculo_Cod, gd.Espectaculo_Descripcion, gd.Espectaculo_Fecha, gd.Espectaculo_Fecha_Venc, gd.Espectaculo_Rubro_Descripcion, gd.Espectaculo_Estado, gd.Ubicacion_Fila, gd.Ubicacion_Asiento, gd.Ubicacion_Sin_numerar, gd.Ubicacion_Precio, gd.Ubicacion_Tipo_Codigo, gd.Ubicacion_Tipo_Descripcion, gd.Cli_Dni, gd.Cli_Apeliido, gd.Cli_Nombre, gd.Cli_Fecha_Nac, gd.Cli_Mail, gd.Cli_Dom_Calle, gd.Cli_Nro_Calle, gd.Cli_Piso, gd.Cli_Depto, gd.Cli_Cod_Postal, gd.Compra_Fecha, gd.Compra_Cantidad, gd.Item_Factura_Monto, gd.Item_Factura_Cantidad, gd.Item_Factura_Descripcion, gd.Factura_Nro, gd.Factura_Fecha, gd.Factura_Total, gd.Forma_Pago_Desc
+		SELECT gd.Espec_Empresa_Razon_Social, gd.Espec_Empresa_Cuit, gd.Espec_Empresa_Fecha_Creacion, gd.Espec_Empresa_Mail, gd.Espec_Empresa_Dom_Calle, gd.Espec_Empresa_Nro_Calle, gd.Espec_Empresa_Piso, gd.Espec_Empresa_Depto, gd.Espec_Empresa_Cod_Postal, gd.Espectaculo_Cod, gd.Espectaculo_Descripcion, gd.Espectaculo_Fecha, gd.Espectaculo_Fecha_Venc, gd.Espectaculo_Rubro_Descripcion, gd.Espectaculo_Estado, gd.Ubicacion_Fila, gd.Ubicacion_Asiento, gd.Ubicacion_Sin_numerar, gd.Ubicacion_Precio, gd.Ubicacion_Tipo_Codigo, gd.Ubicacion_Tipo_Descripcion, gd.Cli_Dni, gd.Cli_Apeliido, gd.Cli_Nombre, gd.Cli_Fecha_Nac, gd.Cli_Mail, gd.Cli_Dom_Calle, gd.Cli_Nro_Calle, gd.Cli_Piso, gd.Cli_Depto, gd.Cli_Cod_Postal, gd.Compra_Fecha, gd.Compra_Cantidad, gd.Item_Factura_Monto, gd.Item_Factura_Cantidad, gd.Item_Factura_Descripcion, gd.Factura_Nro, gd.Factura_Fecha, gd.Factura_Total, gd.Forma_Pago_Desc
 		FROM gd_esquema.Maestra gd
 			
 	OPEN c_maestro
@@ -100,32 +100,32 @@ BEGIN
 				INSERT INTO EL_REJUNTE.Direccion (dire_calle, dire_numero, dire_piso, dire_depto, dire_codigo_postal)
 				VALUES (@Cli_Dom_Calle, @Cli_Nro_Calle, @Cli_Piso, @Cli_Depto, @Cli_Cod_Postal)
 			END
-		END
-		/* Termina el clasificado de Direcciones */
-		/* Ya que los valores son unicos, voy a traerme el ID del campo, en esta instancia tiene que existir si o si */
-		SELECT @ID_Direccion = dire_id 
-		FROM Direccion 
-		WHERE @Cli_Dom_Calle = dire_calle AND 
-			  @Cli_Nro_Calle = dire_numero AND 
-			  @Cli_Piso = dire_piso AND 
-			  @Cli_Depto = dire_depto AND 
-			  @Cli_Cod_Postal = dire_codigo_postal
-			  
-			
-		/*Verifico si el Cliente ya existe o si es Cliente Nuevo*/
-		SELECT @ID_Cliente = clie_id
-		FROM Cliente
-		WHERE clie_nombre = @Cli_Nombre AND
-			  clie_apellido = @Cli_Apellido AND
-			  clie_documento = @Cli_Dni AND
-			  clie_email = @Cli_Mail AND
-			  clie_direccion_id = @ID_Direccion AND
-			  clie_fecha_nacimiento = @Cli_Fecha_Nac
-		IF(@ID_Cliente IS NULL AND @Cli_Nombre IS NOT NULL AND @Cli_Apellido IS NOT NULL)
-		BEGIN
-			/* Hago el Insert de los Clientes, con sus respectivas Direcciones */
-			INSERT INTO EL_REJUNTE.Cliente (clie_nombre, clie_apellido, clie_tipo_documento, clie_documento, clie_cuil, clie_email, clie_telefono, clie_direccion_id, clie_fecha_nacimiento,clie_fecha_creacion, clie_tarjeta_id, clie_habilitado, clie_usuario_id)
-			VALUES (@Cli_Nombre, @Cli_Apellido, 'DNI', @Cli_Dni, '123' , @Cli_Mail, 123, @ID_Direccion, @Cli_Fecha_Nac, GETDATE(), null, 1, null)
+		
+			/* Termina el clasificado de Direcciones */
+			/* Ya que los valores son unicos, voy a traerme el ID del campo, en esta instancia tiene que existir si o si */			  
+				
+			/*Verifico si el Cliente ya existe o si es Cliente Nuevo*/
+			SELECT @ID_Cliente = clie_id
+			FROM Cliente
+			WHERE clie_nombre = @Cli_Nombre AND
+				  clie_apellido = @Cli_Apellido AND
+				  clie_documento = @Cli_Dni AND
+				  clie_email = @Cli_Mail AND
+				  clie_direccion_id = @ID_Direccion AND
+				  clie_fecha_nacimiento = @Cli_Fecha_Nac
+			IF(@ID_Cliente IS NULL)
+			BEGIN
+				SELECT @ID_Direccion = dire_id 
+				FROM Direccion 
+				WHERE @Cli_Dom_Calle = dire_calle AND 
+					  @Cli_Nro_Calle = dire_numero AND 
+					  @Cli_Piso = dire_piso AND 
+					  @Cli_Depto = dire_depto AND 
+					  @Cli_Cod_Postal = dire_codigo_postal
+				/* Hago el Insert de los Clientes, con sus respectivas Direcciones */
+				INSERT INTO EL_REJUNTE.Cliente (clie_nombre, clie_apellido, clie_tipo_documento, clie_documento, clie_cuil, clie_email, clie_telefono, clie_direccion_id, clie_fecha_nacimiento,clie_fecha_creacion, clie_tarjeta_id, clie_habilitado, clie_usuario_id)
+				VALUES (@Cli_Nombre, @Cli_Apellido, 'DNI', @Cli_Dni, '123' , @Cli_Mail, 123, @ID_Direccion, @Cli_Fecha_Nac, GETDATE(), null, 1, null)
+			END
 		END
 		/* Reinicio el ID_Direccion para que si no existe el campo, no me tome el valor del insert anterior */
 		SET @ID_Direccion = NULL
@@ -149,169 +149,187 @@ BEGIN
 				INSERT INTO EL_REJUNTE.Direccion (dire_calle, dire_numero, dire_piso, dire_depto, dire_codigo_postal)
 				VALUES (@Espec_Empresa_Dom_Calle, @Espec_Empresa_Nro_Calle, @Espec_Empresa_Piso, @Espec_Empresa_Depto, @Espec_Empresa_Cod_Postal)
 			END
-		END
+		
 
-		/*Verifico si la Empresa ya existe o si es Empresa Nueva*/
-		SELECT @ID_Empresa = empre_id
-		FROM Empresa
-		WHERE empre_razon_social = @Espec_Empresa_Razon_Social AND
-			  empre_cuit = @Espec_Empresa_Cuit AND
-			  empre_fecha_creacion = @Espec_Empresa_Fecha_Creacion AND
-			  empre_mail = @Espec_Empresa_Mail AND
-			  empre_direccion_id = @ID_Direccion
-		IF(@ID_Empresa IS NULL)
-		BEGIN
-			/* Hago el Insert de las Empresas, con sus respectivas Direcciones */
-			INSERT INTO EL_REJUNTE.Empresa (empre_razon_social, empre_cuit, empre_fecha_creacion, empre_mail, empre_direccion_id, empre_telefono, empre_usuario_id)
-			VALUES (@Espec_Empresa_Razon_Social, @Espec_Empresa_Cuit, @Espec_Empresa_Fecha_Creacion, @Espec_Empresa_Mail, @ID_Direccion, null, null)
+			/*Verifico si la Empresa ya existe o si es Empresa Nueva*/
+			SELECT @ID_Empresa = empre_id
+			FROM Empresa
+			WHERE empre_razon_social = @Espec_Empresa_Razon_Social AND
+				  empre_cuit = @Espec_Empresa_Cuit AND
+				  empre_fecha_creacion = @Espec_Empresa_Fecha_Creacion AND
+				  empre_mail = @Espec_Empresa_Mail AND
+				  empre_direccion_id = @ID_Direccion
+			IF(@ID_Empresa IS NULL)
+			BEGIN
+				SELECT @ID_Direccion = dire_id 
+				FROM Direccion 
+				WHERE @Espec_Empresa_Dom_Calle = dire_calle AND 
+					  @Espec_Empresa_Nro_Calle = dire_numero AND 
+					  @Espec_Empresa_Piso = dire_piso AND 
+					  @Espec_Empresa_Depto = dire_depto AND 
+					  @Espec_Empresa_Cod_Postal = dire_codigo_postal
+				/* Hago el Insert de las Empresas, con sus respectivas Direcciones */
+				INSERT INTO EL_REJUNTE.Empresa (empre_razon_social, empre_cuit, empre_fecha_creacion, empre_mail, empre_direccion_id, empre_telefono, empre_usuario_id)
+				VALUES (@Espec_Empresa_Razon_Social, @Espec_Empresa_Cuit, @Espec_Empresa_Fecha_Creacion, @Espec_Empresa_Mail, @ID_Direccion, null, null)
+			END
 		END
 		/* Reinicio el ID_Direccion para que si no existe el campo, no me tome el valor del insert anterior */
 		SET @ID_Direccion = NULL
 	/* Termina el clasificado de Empresas */
 	
+		IF(@Espectaculo_Cod IS NOT NULL AND @Espectaculo_Descripcion IS NOT NULL AND @Espectaculo_Fecha IS NOT NULL AND @Espectaculo_Fecha_Venc IS NOT NULL AND @Espectaculo_Estado IS NOT NULL)
+		BEGIN
 		/*Verifico si el Espectaculo ya existe o si es Especulo Nuevo*/
-		SELECT @ID_Espectaculo = espec_id
-		FROM Espectaculo
-		WHERE espec_codigo = @Espectaculo_Cod AND
-			  espec_descripcion = @Espectaculo_Descripcion AND
-			  espec_fecha = @Espectaculo_Fecha AND
-			  espec_fecha_venc = @Espectaculo_Fecha_Venc AND
-			  espec_estado = @Espectaculo_Estado
-		IF(@ID_Espectaculo IS NULL)
-		BEGIN
-			INSERT INTO EL_REJUNTE.Espectaculo (espec_codigo , espec_descripcion, espec_fecha, espec_fecha_venc, espec_rubro_id, espec_estado)
-			VALUES (@Espectaculo_Cod, @Espectaculo_Descripcion, @Espectaculo_Fecha, @Espectaculo_Fecha_Venc, null, @Espectaculo_Estado)
+			SELECT @ID_Espectaculo = espec_id
+			FROM Espectaculo
+			WHERE espec_codigo = @Espectaculo_Cod AND
+				  espec_descripcion = @Espectaculo_Descripcion AND
+				  espec_fecha = @Espectaculo_Fecha AND
+				  espec_fecha_venc = @Espectaculo_Fecha_Venc AND
+				  espec_estado = @Espectaculo_Estado
+			IF(@ID_Espectaculo IS NULL)
+			BEGIN
+				INSERT INTO EL_REJUNTE.Espectaculo (espec_codigo , espec_descripcion, espec_fecha, espec_fecha_venc, espec_rubro_id, espec_estado)
+				VALUES (@Espectaculo_Cod, @Espectaculo_Descripcion, @Espectaculo_Fecha, @Espectaculo_Fecha_Venc, null, @Espectaculo_Estado)
+			END
 		END
-	/* Termina el clasificado de Espectaculos */
-	
-		/*Verifico si la Ubicacion ya existe o si es Ubicacion Nueva*/
-		SELECT @ID_Ubicacion = ubica_id
-		FROM Ubicacion
-		WHERE ubica_fila = @Ubicacion_Fila AND
-			  ubica_asiento = @Ubicacion_Asiento AND
-			  ubica_sin_numerar = @Ubicacion_Sin_numerar AND
-			  ubica_precio = @Ubicacion_Precio AND
-			  ubica_tipo_codigo = @Ubicacion_Tipo_Codigo AND 
-			  ubica_tipo_descripcion = @Ubicacion_Tipo_Descripcion
-		IF(@ID_Espectaculo IS NULL)
+		/* Termina el clasificado de Espectaculos */
+		IF(@Ubicacion_Fila IS NOT NULL AND @Ubicacion_Asiento IS NOT NULL AND @Ubicacion_Sin_numerar IS NOT NULL AND @Ubicacion_Precio IS NOT NULL AND @Ubicacion_Tipo_Codigo IS NOT NULL AND @Ubicacion_Tipo_Descripcion IS NOT NULL)
 		BEGIN
-			INSERT INTO EL_REJUNTE.Ubicacion (ubica_fila , ubica_asiento, ubica_sin_numerar, ubica_precio, ubica_tipo_codigo, ubica_tipo_descripcion,ubica_facturada)
-			VALUES (@Ubicacion_Fila, @Ubicacion_Asiento, @Ubicacion_Sin_numerar, @Ubicacion_Precio, @Ubicacion_Tipo_Codigo, @Ubicacion_Tipo_Descripcion, 0)
-		END
-		/*Verifico si la Compra ya existe o si es Compra Nueva*/
-		
-		SELECT @ID_Compra = compra_id
-		FROM Compra
-		WHERE compra_fecha = @Compra_Fecha AND
-			  compra_cantidad = @Compra_Cantidad AND
-			  compra_cliente_id = @ID_Cliente
-			  
-		IF(@ID_Compra IS NULL)
-		BEGIN				
-
-			SELECT @ID_Cliente = clie_id
-			FROM Cliente
-			WHERE clie_nombre = @Cli_Nombre AND
-				clie_apellido = @Cli_Apellido AND
-				clie_documento = @Cli_Dni AND
-				clie_email = @Cli_Mail AND
-				clie_direccion_id = @ID_Direccion AND
-				clie_fecha_nacimiento = @Cli_Fecha_Nac
-			  
+			/*Verifico si la Ubicacion ya existe o si es Ubicacion Nueva*/
 			SELECT @ID_Ubicacion = ubica_id
 			FROM Ubicacion
 			WHERE ubica_fila = @Ubicacion_Fila AND
-			  ubica_asiento = @Ubicacion_Asiento AND
-			  ubica_sin_numerar = @Ubicacion_Sin_numerar AND
-			  ubica_precio = @Ubicacion_Precio AND
-			  ubica_tipo_codigo = @Ubicacion_Tipo_Codigo AND 
-			  ubica_tipo_descripcion = @Ubicacion_Tipo_Descripcion
-			  
-			IF(@ID_Cliente IS NOT NULL AND @ID_Ubicacion IS NOT NULL)
+				  ubica_asiento = @Ubicacion_Asiento AND
+				  ubica_sin_numerar = @Ubicacion_Sin_numerar AND
+				  ubica_precio = @Ubicacion_Precio AND
+				  ubica_tipo_codigo = @Ubicacion_Tipo_Codigo AND 
+				  ubica_tipo_descripcion = @Ubicacion_Tipo_Descripcion
+			IF(@ID_Espectaculo IS NULL)
 			BEGIN
-				INSERT INTO EL_REJUNTE.Compra (compra_fecha , compra_cantidad, compra_cliente_id)
-				VALUES (@Compra_Fecha, @Compra_Cantidad, @ID_Cliente)
-				/* Agrego el campo de la tabla intermedia */
-	
-				SELECT @ID_Compra = compra_id
-				FROM Compra
-				WHERE compra_fecha = @Compra_Fecha AND
-					compra_cantidad = @Compra_Cantidad AND
-					compra_cliente_id = @ID_Cliente
+				INSERT INTO EL_REJUNTE.Ubicacion (ubica_fila , ubica_asiento, ubica_sin_numerar, ubica_precio, ubica_tipo_codigo, ubica_tipo_descripcion,ubica_facturada)
+				VALUES (@Ubicacion_Fila, @Ubicacion_Asiento, @Ubicacion_Sin_numerar, @Ubicacion_Precio, @Ubicacion_Tipo_Codigo, @Ubicacion_Tipo_Descripcion, 0)
+			END
+		END
+		/*Verifico si la Compra ya existe o si es Compra Nueva*/
+		IF(@Compra_Fecha IS NOT NULL AND @Compra_Cantidad IS NOT NULL)
+		BEGIN
+			SELECT @ID_Compra = compra_id
+			FROM Compra
+			WHERE compra_fecha = @Compra_Fecha AND
+				  compra_cantidad = @Compra_Cantidad AND
+				  compra_cliente_id = @ID_Cliente
+				  
+			IF(@ID_Compra IS NULL)
+			BEGIN				
+
+				SELECT @ID_Cliente = clie_id
+				FROM Cliente
+				WHERE clie_nombre = @Cli_Nombre AND
+					clie_apellido = @Cli_Apellido AND
+					clie_documento = @Cli_Dni AND
+					clie_email = @Cli_Mail AND
+					clie_direccion_id = @ID_Direccion AND
+					clie_fecha_nacimiento = @Cli_Fecha_Nac
+				  
+				SELECT @ID_Ubicacion = ubica_id
+				FROM Ubicacion
+				WHERE ubica_fila = @Ubicacion_Fila AND
+				  ubica_asiento = @Ubicacion_Asiento AND
+				  ubica_sin_numerar = @Ubicacion_Sin_numerar AND
+				  ubica_precio = @Ubicacion_Precio AND
+				  ubica_tipo_codigo = @Ubicacion_Tipo_Codigo AND 
+				  ubica_tipo_descripcion = @Ubicacion_Tipo_Descripcion
+				  
+				IF(@ID_Cliente IS NOT NULL AND @ID_Ubicacion IS NOT NULL)
+				BEGIN
+					INSERT INTO EL_REJUNTE.Compra (compra_fecha , compra_cantidad, compra_cliente_id)
+					VALUES (@Compra_Fecha, @Compra_Cantidad, @ID_Cliente)
+					/* Agrego el campo de la tabla intermedia */
 		
-				INSERT INTO EL_REJUNTE.Ubicacion_Compra(ubica_id, compra_id)
-				VALUES(@ID_Ubicacion, @ID_Compra)
+					SELECT @ID_Compra = compra_id
+					FROM Compra
+					WHERE compra_fecha = @Compra_Fecha AND
+						compra_cantidad = @Compra_Cantidad AND
+						compra_cliente_id = @ID_Cliente
+			
+					INSERT INTO EL_REJUNTE.Ubicacion_Compra(ubica_id, compra_id)
+					VALUES(@ID_Ubicacion, @ID_Compra)
+				END
 			END
 		END
 	
 	/* Termina el clasificado de Ubicaciones */
-	
-		SELECT @ID_Factura = fact_id
-		FROM Factura
-		WHERE fact_nro = @Factura_Nro AND
-			  fact_fecha = @Factura_Fecha AND
-			  fact_total = @Factura_Total AND
-			  fact_pago_desc = @Forma_Pago_Desc
-			  
-		IF(@ID_Factura IS NULL )
-		BEGIN
-			SELECT @ID_Cliente = clie_id
-			FROM Cliente
-			WHERE clie_nombre = @Cli_Nombre AND
-				clie_apellido = @Cli_Apellido AND
-				clie_documento = @Cli_Dni AND
-				clie_email = @Cli_Mail AND
-				clie_direccion_id = @ID_Direccion AND
-				clie_fecha_nacimiento = @Cli_Fecha_Nac
-				
-			SELECT @ID_Empresa = empre_id
-			FROM Empresa
-			WHERE empre_razon_social = @Espec_Empresa_Razon_Social AND
-				empre_cuit = @Espec_Empresa_Cuit AND
-				empre_fecha_creacion = @Espec_Empresa_Fecha_Creacion AND
-				empre_mail = @Espec_Empresa_Mail AND
-				empre_direccion_id = @ID_Direccion
-				
-			IF(@ID_Cliente IS NOT NULL AND @ID_Empresa IS NOT NULL AND @Factura_Nro IS NOT NULL)
-			BEGIN
-				INSERT INTO EL_REJUNTE.Factura (fact_nro , fact_fecha, fact_total, fact_pago_desc, fact_cliente_id, fact_empresa_id)
-				VALUES (@Factura_Nro, @Factura_Fecha, @Factura_Total, @Forma_Pago_Desc, @ID_Cliente, @ID_Empresa)
-			END
-		END
-	
-	/* Termina el clasificado de Facturacion */
-	
-		SELECT @ID_Item = item_id
-		FROM Item_Factura
-		WHERE item_monto = @Item_Factura_Monto AND
-			  item_cantidad = @Item_Factura_Cantidad AND
-			  item_descripcion = @Item_Factura_Descripcion	
-	
-		IF(@ID_Item IS NULL )
+		IF(@Factura_Nro IS NOT NULL AND @Factura_Fecha IS NOT NULL AND @Factura_Total IS NOT NULL AND @Forma_Pago_Desc IS NOT NULL)
 		BEGIN
 			SELECT @ID_Factura = fact_id
 			FROM Factura
 			WHERE fact_nro = @Factura_Nro AND
-				fact_fecha = @Factura_Fecha AND
-				fact_total = @Factura_Total AND
-				fact_pago_desc = @Forma_Pago_Desc
-				
-			SELECT @ID_Ubicacion = ubica_id
-			FROM Ubicacion
-			WHERE ubica_fila = @Ubicacion_Fila AND
-			  ubica_asiento = @Ubicacion_Asiento AND
-			  ubica_sin_numerar = @Ubicacion_Sin_numerar AND
-			  ubica_precio = @Ubicacion_Precio AND
-			  ubica_tipo_codigo = @Ubicacion_Tipo_Codigo AND 
-			  ubica_tipo_descripcion = @Ubicacion_Tipo_Descripcion
-				
-			IF(@ID_Factura IS NOT NULL AND @ID_Ubicacion IS NOT NULL)
+				  fact_fecha = @Factura_Fecha AND
+				  fact_total = @Factura_Total AND
+				  fact_pago_desc = @Forma_Pago_Desc
+				  
+			IF(@ID_Factura IS NULL )
 			BEGIN
-				INSERT INTO EL_REJUNTE.Item_Factura (item_monto , item_cantidad, item_descripcion, item_factura_id, item_ubicacion_id)
-				VALUES (@Item_Factura_Monto, @Item_Factura_Cantidad, @Item_Factura_Descripcion, @ID_Factura, @ID_Ubicacion)
+				SELECT @ID_Cliente = clie_id
+				FROM Cliente
+				WHERE clie_nombre = @Cli_Nombre AND
+					clie_apellido = @Cli_Apellido AND
+					clie_documento = @Cli_Dni AND
+					clie_email = @Cli_Mail AND
+					clie_direccion_id = @ID_Direccion AND
+					clie_fecha_nacimiento = @Cli_Fecha_Nac
+					
+				SELECT @ID_Empresa = empre_id
+				FROM Empresa
+				WHERE empre_razon_social = @Espec_Empresa_Razon_Social AND
+					empre_cuit = @Espec_Empresa_Cuit AND
+					empre_fecha_creacion = @Espec_Empresa_Fecha_Creacion AND
+					empre_mail = @Espec_Empresa_Mail AND
+					empre_direccion_id = @ID_Direccion
+					
+				IF(@ID_Cliente IS NOT NULL AND @ID_Empresa IS NOT NULL AND @Factura_Nro IS NOT NULL)
+				BEGIN
+					INSERT INTO EL_REJUNTE.Factura (fact_nro , fact_fecha, fact_total, fact_pago_desc, fact_cliente_id, fact_empresa_id)
+					VALUES (@Factura_Nro, @Factura_Fecha, @Factura_Total, @Forma_Pago_Desc, @ID_Cliente, @ID_Empresa)
+				END
 			END
 		END
 	
+	/* Termina el clasificado de Facturacion */
+		IF(@Item_Factura_Monto IS NOT NULL AND @Item_Factura_Cantidad IS NOT NULL AND @Item_Factura_Descripcion IS NOT NULL)
+		BEGIN
+			SELECT @ID_Item = item_id
+			FROM Item_Factura
+			WHERE item_monto = @Item_Factura_Monto AND
+				  item_cantidad = @Item_Factura_Cantidad AND
+				  item_descripcion = @Item_Factura_Descripcion	
+		
+			IF(@ID_Item IS NULL )
+			BEGIN
+				SELECT @ID_Factura = fact_id
+				FROM Factura
+				WHERE fact_nro = @Factura_Nro AND
+					fact_fecha = @Factura_Fecha AND
+					fact_total = @Factura_Total AND
+					fact_pago_desc = @Forma_Pago_Desc
+					
+				SELECT @ID_Ubicacion = ubica_id
+				FROM Ubicacion
+				WHERE ubica_fila = @Ubicacion_Fila AND
+				  ubica_asiento = @Ubicacion_Asiento AND
+				  ubica_sin_numerar = @Ubicacion_Sin_numerar AND
+				  ubica_precio = @Ubicacion_Precio AND
+				  ubica_tipo_codigo = @Ubicacion_Tipo_Codigo AND 
+				  ubica_tipo_descripcion = @Ubicacion_Tipo_Descripcion
+					
+				IF(@ID_Factura IS NOT NULL AND @ID_Ubicacion IS NOT NULL)
+				BEGIN
+					INSERT INTO EL_REJUNTE.Item_Factura (item_monto , item_cantidad, item_descripcion, item_factura_id, item_ubicacion_id)
+					VALUES (@Item_Factura_Monto, @Item_Factura_Cantidad, @Item_Factura_Descripcion, @ID_Factura, @ID_Ubicacion)
+				END
+			END
+		END
 	
 	/* Reinicio las variables Unicas */
 		SET @ID_Direccion = NULL
