866485d621380b35d0b53b3c1c0e94be90dbc252
unknown <schifferjulian@gmail.com>
2018-11-11 17:35:16 -0300

    agrego varios helpers, y varios templates de objetos

---
+++ src/Menu/OpcionMenu.cs
@@ -0,0 +1,8 @@
+﻿namespace PalcoNet
+{
+    public class OpcionMenu
+    {
+        public int idFuncionalidad { get; set; }
+        public string descripcion { get; set; }
+    }
+}
---
+++ src/Objetos/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PalcoNet;
+
+namespace PalcoNet
+{
+    public class Usuario
+    {
+        public string id { get; set; }
+        public int rol { get; set; }
+        public int hotel { get; set; }
+    }
+}
