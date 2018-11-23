120490b298091a3eed38a9170d291a3a745a7abb
Alan <rivasalagarci@est.frba.utn.edu.ar>
2018-11-23 03:21:41 -0300

    Agrego formulario para seleccionar las ubicaciones

---
+++ src/Comprar/FormComprar.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace PalcoNet.Comprar
+{
+    partial class FormAlta {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAlta));
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblAsiento = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ubica_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblPrecioTotalValue = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblPrecioValue = new System.Windows.Forms.Label();
+            this.cboAsiento = new System.Windows.Forms.ComboBox();
+            this.cboFila = new System.Windows.Forms.ComboBox();
+            this.lblPrecioTotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblFila = new System.Windows.Forms.Label();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCerrar.Location = new System.Drawing.Point(767, 656);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(179, 34);
+            this.btnCerrar.TabIndex = 39;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(113, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(168, 50);
+            this.pictureBox2.TabIndex = 38;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 26);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Formulario de Compra";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnAgregar);
+            this.groupBox2.Controls.Add(this.lblAsiento);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Controls.Add(this.lblPrecioTotalValue);
+            this.groupBox2.Controls.Add(this.txtCantidad);
+            this.groupBox2.Controls.Add(this.lblPrecioValue);
+            this.groupBox2.Controls.Add(this.cboAsiento);
+            this.groupBox2.Controls.Add(this.cboFila);
+            this.groupBox2.Controls.Add(this.lblPrecioTotal);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.lblFila);
+            this.groupBox2.Controls.Add(this.cboTipo);
+            this.groupBox2.Controls.Add(this.lblTipo);
+            this.groupBox2.Location = new System.Drawing.Point(9, 104);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(959, 523);
+            this.groupBox2.TabIndex = 36;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Detalles de la compra";
+            // 
+            // lblAsiento
+            // 
+            this.lblAsiento.AutoSize = true;
+            this.lblAsiento.Location = new System.Drawing.Point(26, 106);
+            this.lblAsiento.Name = "lblAsiento";
+            this.lblAsiento.Size = new System.Drawing.Size(42, 13);
+            this.lblAsiento.TabIndex = 13;
+            this.lblAsiento.Text = "Asiento";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ubica_id});
+            this.dataGridView1.Location = new System.Drawing.Point(39, 305);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(898, 184);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // ubica_id
+            // 
+            this.ubica_id.HeaderText = "Ubicacion ID";
+            this.ubica_id.Name = "ubica_id";
+            // 
+            // lblPrecioTotalValue
+            // 
+            this.lblPrecioTotalValue.AutoSize = true;
+            this.lblPrecioTotalValue.Location = new System.Drawing.Point(132, 220);
+            this.lblPrecioTotalValue.Name = "lblPrecioTotalValue";
+            this.lblPrecioTotalValue.Size = new System.Drawing.Size(34, 13);
+            this.lblPrecioTotalValue.TabIndex = 11;
+            this.lblPrecioTotalValue.Text = "00.00";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(135, 173);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(185, 20);
+            this.txtCantidad.TabIndex = 10;
+            // 
+            // lblPrecioValue
+            // 
+            this.lblPrecioValue.AutoSize = true;
+            this.lblPrecioValue.Location = new System.Drawing.Point(133, 145);
+            this.lblPrecioValue.Name = "lblPrecioValue";
+            this.lblPrecioValue.Size = new System.Drawing.Size(34, 13);
+            this.lblPrecioValue.TabIndex = 9;
+            this.lblPrecioValue.Text = "00.00";
+            // 
+            // cboAsiento
+            // 
+            this.cboAsiento.FormattingEnabled = true;
+            this.cboAsiento.Location = new System.Drawing.Point(135, 104);
+            this.cboAsiento.Name = "cboAsiento";
+            this.cboAsiento.Size = new System.Drawing.Size(183, 21);
+            this.cboAsiento.TabIndex = 8;
+            // 
+            // cboFila
+            // 
+            this.cboFila.FormattingEnabled = true;
+            this.cboFila.Location = new System.Drawing.Point(134, 67);
+            this.cboFila.Name = "cboFila";
+            this.cboFila.Size = new System.Drawing.Size(182, 21);
+            this.cboFila.TabIndex = 7;
+            // 
+            // lblPrecioTotal
+            // 
+            this.lblPrecioTotal.AutoSize = true;
+            this.lblPrecioTotal.Location = new System.Drawing.Point(26, 220);
+            this.lblPrecioTotal.Name = "lblPrecioTotal";
+            this.lblPrecioTotal.Size = new System.Drawing.Size(67, 13);
+            this.lblPrecioTotal.TabIndex = 6;
+            this.lblPrecioTotal.Text = "Precio Total:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cantidad:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Precio individual:";
+            // 
+            // lblFila
+            // 
+            this.lblFila.AutoSize = true;
+            this.lblFila.Location = new System.Drawing.Point(26, 75);
+            this.lblFila.Name = "lblFila";
+            this.lblFila.Size = new System.Drawing.Size(26, 13);
+            this.lblFila.TabIndex = 2;
+            this.lblFila.Text = "Fila:";
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Location = new System.Drawing.Point(135, 27);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(184, 21);
+            this.cboTipo.TabIndex = 1;
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(26, 35);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(31, 13);
+            this.lblTipo.TabIndex = 0;
+            this.lblTipo.Text = "Tipo:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAgregar.Location = new System.Drawing.Point(758, 95);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(179, 34);
+            this.btnAgregar.TabIndex = 40;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            // 
+            // FormAlta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 701);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "FormAlta";
+            this.Text = "FormAlta";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblPrecioTotalValue;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblPrecioValue;
+        private System.Windows.Forms.ComboBox cboAsiento;
+        private System.Windows.Forms.ComboBox cboFila;
+        private System.Windows.Forms.Label lblPrecioTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblFila;
+        private System.Windows.Forms.ComboBox cboTipo;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ubica_id;
+        private System.Windows.Forms.Label lblAsiento;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
---
+++ src/Comprar/FormComprar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PalcoNet.Utils;
+using PalcoNet.Objetos;
+using System.Globalization;
+
+namespace PalcoNet.Comprar
+{
+    public partial class FormAlta : Form
+    {
+        public FormAlta()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
